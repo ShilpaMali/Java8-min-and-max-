f953c238c2bd1397284d96044634a145708e3c75
Shilpa Mali <malishilpa037@gmail.com>
2024-12-28 14:30:32 +0530

    min() method and max() method

---
+++ src/com/max/first/way/Test.java
@@ -0,0 +1,26 @@
+package com.max.first.way;
+
+import java.util.ArrayList;
+
+public class Test {
+	public static void main(String[] args) {
+
+		ArrayList<Employee> al = new ArrayList<Employee>();
+		al.add(new Employee(101, "vinit", 7000));
+		al.add(new Employee(102, "akash", 8000));
+		al.add(new Employee(103, "suresh", 7500));
+		Employee emp=al.stream().max((emp1, emp2) -> {
+			if (emp1.getEmpSalary() == emp2.getEmpSalary()) {
+				return 0;
+			} else if (emp1.getEmpSalary() > emp2.getEmpSalary()) {
+				return 1;
+			} else {
+				return -1;
+			}
+		}).get();
+
+		System.out.println(emp);
+		
+	}
+
+}
---
+++ src/com/min/second/way/Test.java
@@ -0,0 +1,28 @@
+package com.min.second.way;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class Test {
+	public static void main(String[] args) {
+
+		ArrayList<Employee> al = new ArrayList<Employee>();
+		al.add(new Employee(101, "vinit", 7000));
+		al.add(new Employee(102, "akash", 8000));
+		al.add(new Employee(103, "suresh", 7500));
+		Comparator<Employee> c=(emp1, emp2) -> {
+			if (emp1.getEmpSalary() == emp2.getEmpSalary()) {
+				return 0;
+			} else if (emp1.getEmpSalary() > emp2.getEmpSalary()) {
+				return 1;
+			} else {
+				return -1;
+			}
+		};
+
+		Employee emp=al.stream().max(c).get();
+		System.out.println(emp);
+		
+	}
+
+}
